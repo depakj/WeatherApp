7fd6c62259011622e605c108a620303540a7cd5e
Deepak Singh <deeeepak.1singh@gmail.com>
2022-06-05 22:12:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./style.css"
+import "./Btn.css";
+import WheatherCard from "./Weathercard";
+import { FaSearch } from "react-icons/fa";
+const App=()=>{
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+ const [searchValue,setSearchValue] = useState("Faridabad");
+ const [weather, setWheather] = useState({});
+//  const [mainPic,setPic] = useState();
+
+
+//   const getImage = async() =>{
+  
+//        try{
+//          let iurl = `https://api.unsplash.com/search/photos?query=${searchValue}&client_id=k2Bx68wIt6QeG9l9-_GIHQx4-QOdDiuxsfBKVDO7zOo`;
+
+//          const res = await fetch(iurl);
+//          const imageData = await res.json();
+
+//          const{full:pic} = imageData.results[0].urls;
+//          console.log(pic);
+
+//          const imageObject = {
+//            pic
+//           }
+
+//          setPic(pic);
+//        }catch(err){
+//          console.log(err);
+//        }
+//   };
+
+//  useEffect(()=>{
+//    getImage();
+//  },[searchValue]);
+
+  
+ const getWheather = async() => {
+  
+   try{
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid=d8abd77e89ddde5f611c48b324bf7dda`;
+
+        const response = await fetch(url);
+        const data = await response.json();
+     
+         const {humidity, pressure,temp} = data.main;
+         const {country,sunset} = data.sys;
+         const {speed} = data.wind;
+         const {name} =data;
+         const {main : weathermood} = data.weather[0];
+
+        const mywheatherdata = {
+          humidity,
+           pressure,
+           name,
+           temp,
+           weather,
+           country,
+           speed,
+          sunset ,
+          weathermood
+        }
+
+        setWheather(mywheatherdata);
+
+   }catch(err){
+     console.log(err);
+   }
+ };
+
+ useEffect(()=>{
+   getWheather();
+   })
+
+//  style={{backgroundImage:`url(${mainPic})`}}
+
+    return(
+        <>
+        <div className="searchBox"  >
+          <input type="search" id="search" placeholder="search.." className="searchInput" value={searchValue} onChange={(e)=>{setSearchValue(e.target.value)}}/>
+          <button type="button" className="SearchButton" onClick={getWheather}>
+          <FaSearch/>
+          </button>
+        </div>
+
+          <WheatherCard {...weather}/>
+        </>
+    );
+};
 
 export default App;
---
+++ src/Weathercard.js
@@ -0,0 +1,134 @@
+import React, { useEffect } from "react";
+
+
+const Weathercard = ({
+  temp,
+  humidity,
+  pressure,
+  weathermood,
+  name,
+  speed,
+  country,
+  sunset,
+}) => {
+  const [weatherState, setWeatheState] = React.useState("");
+ 
+  //  const getTime = async()=>{
+  //    try{
+       
+  //    const url = `https://www.amdoren.com/api/timezone.php?=MSCHBt7DBgAa5DKTw8HaSdG8rY4Arg=${name}`;
+
+  //     const res = await fetch(url);
+  //     const data = await res.json();
+  //     console.log(data);
+  //     }catch(err){
+  //     console.log(err);
+  //    }
+  //  };
+
+    // useEffect(()=>{
+    //   getTime();
+    // })
+ 
+
+  useEffect(() => {
+    if (weathermood) {
+      switch (weathermood) {
+        case "Clouds":
+          setWeatheState("wi-day-cloudy");
+          break;
+        case "Haze":
+          setWeatheState("wi-fog");
+          break;
+        case "Clear":
+          setWeatheState("wi-day-sunny");
+          break;
+        case "Mist":
+          setWeatheState("wi-dust");
+          break;
+
+        default:
+          setWeatheState("wi-day-sunny");
+          break;
+      }
+    }
+  }, [weathermood]);
+
+  // converting the seconds into time
+  let sec = sunset;
+  let date = new Date(sec * 1000);
+  let timeStr = `${date.getHours()}:${date.getMinutes()}`;
+  return (
+    <>
+      <article className="widget">
+        <div className="weatherIcon">
+          <i className={`wi ${weatherState}`}></i>
+        </div>
+
+        <div className="weatherInfo">
+          <div className="temperature">
+            <span>{temp}&deg;</span>
+          </div>
+
+          <div className="description">
+            <div className="weatherCondition">{weathermood}</div>
+            <div className="place">
+              {name}, {country}
+            </div>
+          </div>
+        </div>
+
+        <div className="date"> {new Date().toLocaleString()} </div>
+
+        {/* our 4column section  */}
+        <div className="extra-temp">
+          <div className="temp-info-minmax">
+            <div className="two-sided-section">
+              <p>
+                <i className={"wi wi-sunset"}></i>
+              </p>
+              <p className="extra-info-leftside">
+                {timeStr} PM <br />
+                Sunset
+              </p>
+            </div>
+
+            <div className="two-sided-section">
+              <p>
+                <i className={"wi wi-humidity"}></i>
+              </p>
+              <p className="extra-info-leftside">
+                {humidity} <br />
+                Humidity
+              </p>
+            </div>
+          </div>
+
+          <div className="weather-extra-info">
+            <div className="two-sided-section">
+              <p>
+                <i className={"wi wi-rain"}></i>
+              </p>
+              <p className="extra-info-leftside">
+                {pressure} <br />
+                Pressure
+              </p>
+            </div>
+
+            <div className="two-sided-section">
+              <p>
+                <i className={"wi wi-strong-wind"}></i>
+              </p>
+              <p className="extra-info-leftside">
+                {speed} <br />
+                Speed
+              </p>
+            </div>
+          </div>
+        </div>
+      </article>
+    </>
+  );
+};
+
+export default Weathercard;
